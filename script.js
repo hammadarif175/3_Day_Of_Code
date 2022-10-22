5c60a094c5e0388d768455ebe1e732880d34f5fc
hammadarif175 <110495055+hammadarif175@users.noreply.github.com>
2022-10-22 08:08:47 +0500

    Project Completed v1

---
+++ script.js
@@ -0,0 +1,36 @@
+const open = document.getElementById('open')
+const close =  document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () =>{
+    container.classList.add('show-nav');
+})
+
+close.addEventListener('click', () =>{
+    container.classList.remove('show-nav');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListner('click' , () => {
+//     container.classList.add('show-nav')
+//     console.log('open')
+
+// })
+// close.addEventListner('click' , () => {
+//     container.classList.remove('show-nav')
+//     console.log('close')
+// })
